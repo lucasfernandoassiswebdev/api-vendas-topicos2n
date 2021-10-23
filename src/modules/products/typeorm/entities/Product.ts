0ef00632c4f7df5ce1e1c60f6e12b635cb8b881a
Lucas Assis <lucasassis@lojadomecanico.com.br>
2021-10-23 11:00:46 -0300

    master - 23/10 - adicionando primeiro model e estrutura que o projeto ira seguir

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('products')
+class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column()
+    name: string
+
+    @Column('decimal')
+    price: number
+
+    @Column('int')
+    quantity: number
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @CreateDateColumn({ name: 'updated_at' })
+    updatedAt: Date
+}
+
+export default Product
---
+++ src/shared/typeorm/migrations/1634991148646-PostRefactoring.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class PostRefactoring1634991148646 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'products',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()'
+                },
+                {
+                    name: 'name',
+                    type: 'varchar'
+                },
+                {
+                    name: 'price',
+                    type: 'decimal'
+                },
+                {
+                    name: 'quantity',
+                    type: 'int'
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp with time zone',
+                    default: 'now()'
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp with time zone',
+                    default: 'now()'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('products')
+    }
+
+}
