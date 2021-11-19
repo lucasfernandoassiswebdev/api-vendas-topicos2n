7a855a06b0e78fb40c553db63eef564c6e3cb47b
Lucas Assis <lucasassis@lojadomecanico.com.br>
2021-11-18 21:59:37 -0300

    master - aula 18/11 - implementando autenticaçao jwt

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import productRouter from "../../../modules/products/routes/routes.products"
 import userRouter from "../../../modules/users/routes/routes.user"
+import sessionRouter from "../../../modules/users/routes/routes.session"
 
 const routes = Router()
 
 routes.use("/product", productRouter)
 routes.use("/user", userRouter)
+routes.use("/session", sessionRouter)
 
 export default routes
